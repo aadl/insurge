1dd9cfe88a81faf51aea9b06619786fae010e9a9
Ryan Eby <ryaneby@gmail.com>
2010-05-05 17:03:18 -0400

    content_type field what will refer to content_type table ids. will be used in tagging non-bib items

--- sql/scas_insurge.sql
+++ sql/scas_insurge.sql
@@ -104,6 +104,7 @@ CREATE TABLE IF NOT EXISTS `insurge_tags` (
   `repos_id` char(24) default NULL,
   `group_id` char(12) default NULL,
   `uid` varchar(12) default NULL,
+  `content_type` int(12) NOT NULL,
   `content_id` int(12) NOT NULL,
   `tag` varchar(256) NOT NULL,
   `namespace` varchar(256) NOT NULL,
